c207fb4925f24223de4a497ea032bd04339ff355
Qianchuan Ye <yeqianchuan@gmail.com>
2022-08-21 15:38:22 -0400

    Restructure for easier testing

--- app/Main.hs
+++ app/Main.hs
@@ -1,44 +1,6 @@
-{-# LANGUAGE ApplicativeDo #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
-
 module Main (main) where
 
-import Options.Applicative
-import Taype
-import Taype.Environment
-
-opts :: Parser Options
-opts = do
-  optFile <-
-    strArgument $
-      metavar "FILE" <> help "Taype source file"
-  optInternalNames <-
-    switch $
-      long "internal-names" <> short 'i'
-        <> help "Whether to print the internal names for variables"
-  optNamePrefix <-
-    strOption $
-      long "prefix" <> short 'p' <> metavar "PREFIX"
-        <> value "$"
-        <> showDefault
-        <> help "Prefix to the internal names"
-  optPrintLabels <-
-    switch $
-      long "print-labels" <> short 'l'
-        <> help "Whether to print the leakage labels"
-  optPrintTokens <-
-    switch $
-      long "print-tokens" <> short 't'
-        <> help "Whether to print tokens (for internal debugging)"
-  optPrintSource <-
-    switch $
-      long "print-source" <> short 's'
-        <> help "Whether to print the source code (for internal debugging)"
-  return Options {..}
+import qualified Taype
 
 main :: IO ()
-main = run =<< execParser (info (opts <**> helper) helpMod)
-  where
-    helpMod =
-      fullDesc <> header "taype - a programming language with data types and tape"
+main = Taype.main
--- src/Taype.hs
+++ src/Taype.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
 -- |
@@ -9,10 +11,11 @@
 --
 -- Top-level functions.
 module Taype
-  ( run,
+  ( main,
   )
 where
 
+import Options.Applicative
 import Prettyprinter.Render.Text
 import Taype.Cute
 import Taype.Environment
@@ -34,3 +37,38 @@ process file code options@Options {..} = do
   (defs, gctx) <- hoistEither $ parse tokens
   -- Always print out the source code for now
   lift $ putDoc $ cuteDefs defs Env {..}
+
+main :: IO ()
+main = run =<< execParser (info (opts <**> helper) helpMod)
+  where
+    helpMod =
+      fullDesc <> header "taype - a programming language with data types and tape"
+
+opts :: Parser Options
+opts = do
+  optFile <-
+    strArgument $
+      metavar "FILE" <> help "Taype source file"
+  optInternalNames <-
+    switch $
+      long "internal-names" <> short 'i'
+        <> help "Whether to print the internal names for variables"
+  optNamePrefix <-
+    strOption $
+      long "prefix" <> short 'p' <> metavar "PREFIX"
+        <> value "$"
+        <> showDefault
+        <> help "Prefix to the internal names"
+  optPrintLabels <-
+    switch $
+      long "print-labels" <> short 'l'
+        <> help "Whether to print the leakage labels"
+  optPrintTokens <-
+    switch $
+      long "print-tokens" <> short 't'
+        <> help "Whether to print tokens (for internal debugging)"
+  optPrintSource <-
+    switch $
+      long "print-source" <> short 's'
+        <> help "Whether to print the source code (for internal debugging)"
+  return Options {..}
