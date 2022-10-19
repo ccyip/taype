d9f0756613dee998d9a70a51ed011a1c8e6458bf
Qianchuan Ye <yeqianchuan@gmail.com>
2022-10-19 00:22:26 -0400

    Flesh out list examples

---
+++ examples/list/test_hamming_distance.ml
@@ -0,0 +1,30 @@
+open Driver
+open Prelude
+open Common
+open Sexplib
+open List
+open List_conceal
+open List_reveal
+open List_helper
+
+let _ =
+  parse_options ();
+  setup_driver_simple ();
+
+  let n = get_public_int () in
+  let size = obliv_list n in
+  let xs = get_private (mylist_of_sexp_check n) (private_s_list n) size in
+  let ys = get_private (mylist_of_sexp_check n) (private_s_list n) size in
+  let expected = get_expected Conv.int_of_sexp in
+
+  collect_stat ();
+
+  let obliv_res = obliv_hamming_distance n xs ys in
+
+  record_stat ();
+
+  let res = unsafe_r_int obliv_res in
+
+  finalize_driver ();
+
+  expected = res |> print_result
