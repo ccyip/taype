94df1a8a70d6a0046b7c348c1dea73f08920b804
Qianchuan Ye <yeqianchuan@gmail.com>
2022-11-02 02:44:59 -0400

    Fix example

--- examples/tree/test_lookup.ml
+++ examples/tree/test_lookup.ml
@@ -18,14 +18,12 @@ let _ =
 
   collect_stat ();
 
-  (* let obliv_res = obliv_lookup n t x in *)
-  let obliv_res = leaky_ap leaky_if_of_tree (leaky_ap leaky_if_of_arrow r_tree n) t in
+  let obliv_res = obliv_lookup n t x in
 
   record_stat ();
 
-  (* let res = unsafe_r_bool obliv_res in *)
+  let res = unsafe_r_bool obliv_res in
 
   finalize_driver ();
 
-  (* expected = res |> print_result *)
-  expected = expected |> print_result
+  expected = res |> print_result
