39f7952fc80a78edc9a513acb8172b0d74f2887c
Qianchuan Ye <yeqianchuan@gmail.com>
2023-02-22 23:17:53 -0500

    Add k-mean example

---
+++ examples/kmean/test_kmean.ml
@@ -0,0 +1,37 @@
+open Driver
+open Prelude
+open Common
+open Sexplib
+open Kmean
+open Kmean_conceal
+open Kmean_reveal
+open Kmean_helper
+
+let _ =
+  parse_options ();
+  setup_driver_simple ();
+
+  let n_iter = get_public_int () in
+  let n_clusters = get_public_int () in
+  let dim = get_public_int () in
+  let k1 = get_public_int () in
+  let obliv_xs1 = get_private
+      (vlist_of_sexp_check (dim, k1))
+      (private_s_vlist (dim, k1)) (obliv_vlist (dim, k1)) in
+  let k2 = get_public_int () in
+  let obliv_xs2 = get_private
+      (vlist_of_sexp_check (dim, k2))
+      (private_s_vlist (dim, k2)) (obliv_vlist (dim, k2)) in
+
+  collect_stat ();
+
+  let obliv_res = obliv_kmean n_iter n_clusters dim k1 k2 obliv_xs1 obliv_xs2 in
+
+  record_stat ();
+
+  let res = unsafe_r_vec (k1+k2) obliv_res in
+
+  finalize_driver ();
+
+  print_stat ();
+  vec_to_sexp res |> print_sexp
