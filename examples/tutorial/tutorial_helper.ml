cc32c327c58ba556c19fbe7f142dc280e6182645
Qianchuan Ye <yeqianchuan@gmail.com>
2023-03-02 23:29:11 -0500

    More comments

--- examples/tutorial/tutorial_helper.ml
+++ examples/tutorial/tutorial_helper.ml
@@ -2,6 +2,11 @@ open Sexplib
 open Tutorial
 open Common
 
+(* This file implements the conversion between s-expression and the list data in
+   the Taype source code. When we convert from s-expression to list, we also
+   check if the given public view is valid, i.e. if it is not smaller than the
+   actual length of the list. *)
+
 let rec mylist_to_list = function
   | Nil -> []
   | Cons (x, xs) -> x :: mylist_to_list xs
