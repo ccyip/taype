ff9b1f31e8f2b2b0baa6bbaa610f1e804237695f
Qianchuan Ye <yeqianchuan@gmail.com>
2023-05-22 18:54:44 -0400

    Revise decision tree example

--- examples/dtree/test_dtree_max.ml
+++ examples/dtree/test_dtree_max.ml
@@ -1,36 +1,37 @@
-(* open Driver *)
-(* open Prelude *)
-(* open Common *)
-(* open Sexplib *)
-(* open Dtree *)
-(* open Dtree_conceal *)
-(* open Dtree_reveal *)
-(* open Dtree_helper *)
+open Sexplib
+open Common
+module Driver = (val parse_options ())
+open Driver
+open Setup (Driver)
+open Dtree.M (Driver)
+open Dtree_helper.M (Driver)
 
-(* let _ = *)
-(*   parse_options (); *)
-(*   setup_driver_simple (); *)
+let () =
+  setup_driver_simple ();
 
-(*   let kt = get_public_int () in *)
-(*   let t = get_private *)
-(*       (dtree_of_sexp_check_max kt) *)
-(*       (private_s_dtree_max kt) *)
-(*       (obliv_dtree_max kt) in *)
-(*   let k = get_public_int () in *)
-(*   let xs = get_private *)
-(*       (features_of_sexp_check k) *)
-(*       (private_s_features k) *)
-(*       (obliv_features k) in *)
+  let kt = get_public_int () in
+  let t =
+    get_private
+      (dtree_of_sexp_check_max kt)
+      (Conceal.obliv_dtree_max_s kt)
+      (Conceal.obliv_dtree_max_s_for kt)
+  in
+  let k = get_public_int () in
+  let xs =
+    get_private (features_of_sexp_check k)
+      (Conceal.obliv_features_s k)
+      (Conceal.obliv_features_s_for k)
+  in
 
-(*   collect_stat (); *)
+  collect_stat ();
 
-(*   let obliv_res = obliv_decide_max kt k t xs in *)
+  let obliv_res = obliv_decide_max t xs in
 
-(*   record_stat (); *)
+  record_stat ();
 
-(*   let res = unsafe_r_int obliv_res in *)
+  let res = Reveal.obliv_int_r obliv_res in
 
-(*   finalize_driver (); *)
+  finalize_driver ();
 
-(*   print_stat (); *)
-(*   Conv.sexp_of_int res |> print_sexp *)
+  print_stat ();
+  Conv.sexp_of_int res |> print_sexp
--- examples/dtree/test_dtree_spine.ml
+++ examples/dtree/test_dtree_spine.ml
@@ -1,36 +1,37 @@
-(* open Driver *)
-(* open Prelude *)
-(* open Common *)
-(* open Sexplib *)
-(* open Dtree *)
-(* open Dtree_conceal *)
-(* open Dtree_reveal *)
-(* open Dtree_helper *)
+open Sexplib
+open Common
+module Driver = (val parse_options ())
+open Driver
+open Setup (Driver)
+open Dtree.M (Driver)
+open Dtree_helper.M (Driver)
 
-(* let _ = *)
-(*   parse_options (); *)
-(*   setup_driver_simple (); *)
+let () =
+  setup_driver_simple ();
 
-(*   let s = get_public spine_of_sexp in *)
-(*   let t = get_private *)
-(*       (dtree_of_sexp_check_spine s) *)
-(*       (private_s_dtree_spine s) *)
-(*       (obliv_dtree_spine s) in *)
-(*   let k = get_public_int () in *)
-(*   let xs = get_private *)
-(*       (features_of_sexp_check k) *)
-(*       (private_s_features k) *)
-(*       (obliv_features k) in *)
+  let s = get_public spine_of_sexp in
+  let t =
+    get_private
+      (dtree_of_sexp_check_spine s)
+      (Conceal.obliv_dtree_spine_s s)
+      (Conceal.obliv_dtree_spine_s_for s)
+  in
+  let k = get_public_int () in
+  let xs =
+    get_private (features_of_sexp_check k)
+      (Conceal.obliv_features_s k)
+      (Conceal.obliv_features_s_for k)
+  in
 
-(*   collect_stat (); *)
+  collect_stat ();
 
-(*   let obliv_res = obliv_decide_spine s k t xs in *)
+  let obliv_res = obliv_decide_spine t xs in
 
-(*   record_stat (); *)
+  record_stat ();
 
-(*   let res = unsafe_r_int obliv_res in *)
+  let res = Reveal.obliv_int_r obliv_res in
 
-(*   finalize_driver (); *)
+  finalize_driver ();
 
-(*   print_stat (); *)
-(*   Conv.sexp_of_int res |> print_sexp *)
+  print_stat ();
+  Conv.sexp_of_int res |> print_sexp
