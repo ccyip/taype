c76cdd87bf8757415feddaa1badcf18f93579708
Qianchuan Ye <yeqianchuan@gmail.com>
2023-07-21 18:17:21 -0400

    Add coil example

---
+++ examples/coil/test_linear_oram_offline.ml
@@ -0,0 +1,18 @@
+open Taype_driver
+open Taype_driver_coil
+open Driver
+open Coil.M (Driver)
+
+let () =
+  setup_driver "127.0.0.1" 12345 Party.Public;
+
+  let n = 8 in
+
+  let xs = Conceal.obliv_list_eq_s_for n Party.Trusted in
+  let i = Conceal.obliv_int_s_for Party.Trusted in
+
+  let res = obliv_lookup xs i in
+
+  compile_coil_simple "linear_oram" res;
+
+  finalize_driver ()
