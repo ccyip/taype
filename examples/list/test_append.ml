6409d3a78172b8cef3804dc3e8573c61ff8bae8e
Qianchuan Ye <yeqianchuan@gmail.com>
2023-10-05 03:57:39 -0400

    Add more benchmark

---
+++ examples/list/test_append.ml
@@ -0,0 +1,31 @@
+open Common
+module Driver = (val parse_options ())
+open Driver
+open Setup (Driver)
+open List.M (Driver)
+open List_helper.M (Driver)
+
+let () =
+  setup_driver_simple ();
+  let n = get_public_int () in
+  let xs =
+    get_private (mylist_of_sexp_check n) (Conceal.obliv_list_s n)
+      (Conceal.obliv_list_s_for n)
+  in
+  let ys =
+    get_private (mylist_of_sexp_check n) (Conceal.obliv_list_s n)
+      (Conceal.obliv_list_s_for n)
+  in
+  let expected = get_expected mylist_of_sexp in
+
+  collect_stat ();
+
+  let res = obliv_append xs ys in
+
+  record_stat ();
+
+  let res = Reveal.obliv_list_r res in
+
+  finalize_driver ();
+
+  expected = res |> print_result
