76a2a43dc92b12f4088b976998ca218ddc5038eb
Qianchuan Ye <yeqianchuan@gmail.com>
2023-12-26 01:34:08 -0500

    Update benchmark script

--- bench.sh
+++ bench.sh
@@ -2,22 +2,19 @@
 
 set -euo pipefail
 
-# FIXME: is there a better way?
-# export DYLD_LIBRARY_PATH=$OPAM_SWITCH_PREFIX/lib
-
 function go() {
   cabal run shake -- --round=5 $@
 }
 
 # No smart array
-# FIXME: we need to run this separately after installing the right driver
-# go --no-smart-array --out-dir=examples/output-no-smart-array run/list
-# go --no-smart-array --out-dir=examples/output-no-smart-array run/tree
+go clean
+go --naive --out-dir=examples/output-no-smart-array run/list
+go --naive --out-dir=examples/output-no-smart-array run/tree
 
 # No reshape guard
 go clean
-TAYPE_FLAGS=--fno-guard-reshape go --out-dir=examples/output-no-reshape-guard run/list
-TAYPE_FLAGS=--fno-guard-reshape go --out-dir=examples/output-no-reshape-guard run/tree
+TAYPE_FLAGS=--fno-reshape-guard go --out-dir=examples/output-no-reshape-guard run/list
+TAYPE_FLAGS=--fno-reshape-guard go --out-dir=examples/output-no-reshape-guard run/tree
 
 # No memo
 go clean
@@ -33,7 +30,10 @@ go --out-dir=examples/output-best run/tree
 go --out-dir=examples/output-best run/list-memo
 go --out-dir=examples/output-best run/tree-memo
 
-# Compilation
+# Compilation (collect 5 runs)
+#
+# Do not need to clean after each compilation. Taype compiler will always get
+# rerun, while OCaml compiler will not.
 go clean
 go --out-dir=examples/output-compile build
 go --out-dir=examples/output-compile build
