e1af273dadb2929ef6db0d325e0d3b3b9e18a140
Qianchuan Ye <yeqianchuan@gmail.com>
2023-12-28 02:59:41 -0500

    Allow setting round in benchmark script

--- bench.sh
+++ bench.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+ROUND="${1:-5}"
+
 set -euo pipefail
 
 cabal run shake
 
-cabal run shake -- --round=5 --out-dir=examples/output-old-sa run/list
-cabal run shake -- --round=5 --out-dir=examples/output-old-sa run/tree
+cabal run shake -- --round=$ROUND --out-dir=examples/output-old-sa run/list
+cabal run shake -- --round=$ROUND --out-dir=examples/output-old-sa run/tree
