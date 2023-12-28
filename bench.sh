8d3e351ad066c367c2934c9716ec43e0067c2598
Qianchuan Ye <yeqianchuan@gmail.com>
2023-12-28 02:58:33 -0500

    Allow setting round in benchmark script

--- bench.sh
+++ bench.sh
@@ -1,42 +1,46 @@
 #!/bin/bash
 
+ROUND="${1:-5}"
+
 set -euo pipefail
 
+function clean() {
+  cabal run shake -- clean
+}
+
 function go() {
-  cabal run shake -- --round=5 $@
+  cabal run shake -- --round=$ROUND $@
 }
 
 # No smart array
-go clean
+clean
 go --naive --out-dir=examples/output-no-smart-array run/list
 go --naive --out-dir=examples/output-no-smart-array run/tree
 
 # No reshape guard
-go clean
+clean
 TAYPE_FLAGS=--fno-reshape-guard go --out-dir=examples/output-no-reshape-guard run/list
 TAYPE_FLAGS=--fno-reshape-guard go --out-dir=examples/output-no-reshape-guard run/tree
 
 # No memo
-go clean
+clean
 TAYPE_FLAGS=--fno-memo go --out-dir=examples/output-no-memo run/list
 TAYPE_FLAGS=--fno-memo go --out-dir=examples/output-no-memo run/tree
 TAYPE_FLAGS=--fno-memo go --out-dir=examples/output-no-memo run/list-memo
 TAYPE_FLAGS=--fno-memo go --out-dir=examples/output-no-memo run/tree-memo
 
 # All optimization enabled
-go clean
+clean
 go --out-dir=examples/output-best run/list
 go --out-dir=examples/output-best run/tree
 go --out-dir=examples/output-best run/list-memo
 go --out-dir=examples/output-best run/tree-memo
 
-# Compilation (collect 5 runs)
+# Compilation
 #
 # Do not need to clean after each compilation. Taype compiler will always get
 # rerun, while OCaml compiler will not.
-go clean
-go --out-dir=examples/output-compile build
-go --out-dir=examples/output-compile build
-go --out-dir=examples/output-compile build
-go --out-dir=examples/output-compile build
-go --out-dir=examples/output-compile build
+clean
+for ((n=0; n<$ROUND; n++)); do
+  cabal run shake -- --out-dir=examples/output-compile build
+done
